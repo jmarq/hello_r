52781cd7c4ee14bde6eddbf8b4569b12f14be05a
Jimbo <marquardtjames@gmail.com>
2021-05-04 20:38:04 -0500

    initial project setup for experimenting with R via Rstudio. simple little plot script.

---
+++ simple_plot.r
@@ -0,0 +1,6 @@
+# one can run this script with Rscript simple_plot.r from the command line
+# it seems to create an Rplots.pdf file that contains the resulting visualization
+
+nums = c(1,2,3)
+doubled_nums = nums * 2
+plot(nums,doubled_nums)
