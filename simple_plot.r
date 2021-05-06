3b9da400285bfff26bd45baae3edb978795c3e7c
Jimbo <marquardtjames@gmail.com>
2021-05-05 22:57:47 -0500

    txtplot

--- simple_plot.r
+++ simple_plot.r
@@ -1,7 +1,8 @@
 # one can run this script with Rscript simple_plot.r from the command line
 # it seems to create an Rplots.pdf file that contains the resulting visualization
-
+library(txtplot)
 nums = c(1,2,3)
 doubled_nums <- nums * 2
 plot(nums,doubled_nums)
+txtplot(nums,doubled_nums)
 print(sum(doubled_nums))
